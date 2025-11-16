67c9f7704cfe24438c81cc2c5b539ae9838fca01
Scamir <gderamchi@gmail.com>
2025-11-16 05:51:53 +0100

    v0.1.1

--- scrapers/__init__.py
+++
@@ -1,7 +0,0 @@
-from .news_scraper import NewsScraper
-from .youtube_scraper import YouTubeScraper
-from .twitter_scraper import TwitterScraper
-from .reddit_scraper import RedditScraper
-from .forum_scraper import ForumScraper
-
-__all__ = ['NewsScraper', 'YouTubeScraper', 'TwitterScraper', 'RedditScraper', 'ForumScraper']
